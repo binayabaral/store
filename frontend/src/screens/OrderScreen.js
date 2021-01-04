67975fa779a3718bfeb6e5225384f76b1d965f67
Binaya Baral <binaya.baral@p2h.com>
2021-01-04 18:58:24 +0545

    Paypal Button Fix for Netlify

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -43,7 +43,7 @@ const OrderScreen = ({ match, history }) => {
 		}
 
 		const addPayPalScript = async () => {
-			const { data: clientId } = await axios.get('/api/config/paypal');
+			const { data: clientId } = await axios.get('https://baralstore.herokuapp.com/api/config/paypal');
 			const script = document.createElement('script');
 			script.type = 'text/javascript';
 			script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`;
