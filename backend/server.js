23ef5b21e01757c798480400a884e5cffac8702a
Binaya Baral <binaya.baral@p2h.com>
2021-01-04 16:53:42 +0545

    CORS Enabled

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,12 @@ if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
 
+app.use(function (req, res, next) {
+	res.header('Acess-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
