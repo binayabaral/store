3b2e8d300b0e5adca7bbf01f139cc1378c89e9cc
Binaya Baral <binaya.baral@p2h.com>
2021-01-10 14:01:38 +0545

    Header and footer redesign

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -11,14 +11,17 @@ const SearchBox = ({ history }) => {
 		} else {
 			history.push('/');
 		}
+		setKeyword('');
 	};
 
 	return (
-		<Form onSubmit={submitHandler} inline className="d-flex justify-content-center">
-			<Form.Control type="text" name="q" onChange={e => setKeyword(e.target.value)} placeholder="Search Products..." className="mb-5 mb-sm-0"></Form.Control>
-			<Button type="submit" variant="outline-light py-4">
-				Search
-			</Button>
+		<Form onSubmit={submitHandler} inline className="d-flex justify-content-center flex-grow-1 header-search-form">
+			<div className="input-holder">
+				<Form.Control type="text" name="q" value={keyword} onChange={e => setKeyword(e.target.value)} placeholder="Search Products..."></Form.Control>
+				<Button type="submit" variant="primary">
+					<i className="fa fa-search"></i>
+				</Button>
+			</div>
 		</Form>
 	);
 };
